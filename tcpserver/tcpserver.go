3175eb3defdd971938bd11069c7c060ade922b08
Christian Adell <chadell@gmail.com>
2017-10-03 16:50:32 +0200

    Basic Unit/Functional Testing and Continuous Integration (#6)
    * basic travis trigger
    
    make cibuild executable
    
    * basic GO unitesting
    
    * get rid of python tcpserver by using a go one
    
    * add basic testing structure
    
    * add basic testing for tcpserver package
    
    * add basic testing for tcpmaxconn
    
    * solve pr review
    
    * debug bash script
    
    * version Go
    
    * create a subshell for testing
    
    * rename main go programs
    
    * change #/bin/bash to #/usr/bin/env python bash
    
    * ignore go binary
    
    * small refinements
    
    * get rid of unnecessary main package for tcpserver
    
    * get rid of import

---
+++ tcpserver/tcpserver.go
@@ -0,0 +1,75 @@
+package tcpserver
+
+import (
+    "strconv"
+    "net"
+    "log"
+    "time"
+    "bufio"
+	"io"
+	"fmt"
+)
+
+type Handler struct {
+    conn   net.Conn
+    closed chan bool
+}
+
+func (h *Handler) Listen() { // listen connection for incomming data
+    defer h.conn.Close()
+    bf := bufio.NewReader(h.conn)
+    for {
+		line, _, err := bf.ReadLine()
+		log.Println(line)
+        if err != nil {
+            if err == io.EOF {
+                log.Println("End connection")
+            }
+            h.closed <- true // send to dispatcher, that connection is closed
+            return
+        }
+    }
+}
+
+type Dispatcher struct {
+    Handlers map[string]*Handler //`type:"map[ip]*Handler"`
+}
+
+func (d *Dispatcher) AddHandler(conn net.Conn) {
+    addr := conn.RemoteAddr().String()
+    handler := &Handler{conn, make(chan bool, 1)}
+    d.Handlers[addr] = handler
+
+    go handler.Listen()
+
+    <-handler.closed // when connection closed, remove handler from handlers
+    delete(d.Handlers, addr)
+}
+
+func (d *Dispatcher) ListenHandlers(port int) error {
+    sport := strconv.Itoa(port)
+
+    ln, err := net.Listen("tcp", ":" + sport)
+    if err != nil {
+        log.Println(err)
+        return err
+    }
+
+    defer ln.Close()
+
+    for {
+        conn, err := ln.Accept()
+        if err != nil {
+            log.Println(err)
+            continue
+        }
+		fmt.Println(conn.RemoteAddr())
+
+        tcpconn := conn.(*net.TCPConn)
+        tcpconn.SetKeepAlive(true)
+        tcpconn.SetKeepAlivePeriod(10 * time.Second)
+
+        go d.AddHandler(conn)
+    }
+}
+
---
+++ tcpserver/tcpserver_test.go
@@ -0,0 +1,34 @@
+package tcpserver
+
+import (
+	"testing"
+	"time"
+	"sync"
+	"net"
+)
+
+var once sync.Once
+
+func tcpServer(t *testing.T) func () {
+	return func() {
+		t.Log("Starting TCP server")
+		dispatcher := &Dispatcher{make(map[string]*Handler)}		
+		if err := dispatcher.ListenHandlers(8888); err != nil {
+			t.Error("Could not start the TCP server", err)
+			return
+		} else {
+			t.Log("TCP server started")
+		}
+	}
+}
+func TestTcpServer(t *testing.T) {
+	t.Log("Testing TCP server")
+	f := tcpServer(t)
+	go once.Do(f)
+	time.Sleep(1 * time.Second)
+	conn, err := net.Dial("tcp", "127.0.0.1:8888")
+	if err != nil {
+		t.Fatal("Could not connect to TCP server", err)
+	}
+	conn.Close()
+}
