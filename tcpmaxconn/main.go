34ff2a6100c196f56bd75a4752b3b22d6ccb13d7
Christian Adell Querol <christian.adell@schibsted.com>
2017-10-21 11:12:53 +0200

    fix GO typos

--- tcpmaxconn/main.go
+++ tcpmaxconn/main.go
@@ -2,55 +2,55 @@ package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"net"
 	"runtime"
-	"sync"
 	"strconv"
+	"sync"
 	"time"
-	"flag"
 )
 
-func connection_handler(id int, host string, port int, wg *sync.WaitGroup) error{
-	fmt.Println("\t runner "+strconv.Itoa(id)+" is initiating a connection")
+func connectionHandler(id int, host string, port int, wg *sync.WaitGroup) error {
+	fmt.Println("\t runner " + strconv.Itoa(id) + " is initiating a connection")
 	conn, err := net.Dial("tcp", host+":"+strconv.Itoa(port))
 	if err != nil {
 		fmt.Println(err)
 		return err
 	}
-	fmt.Println("\t runner "+strconv.Itoa(id)+" established the connection")
+	fmt.Println("\t runner " + strconv.Itoa(id) + " established the connection")
 	connBuf := bufio.NewReader(conn)
-	for{
+	for {
 		str, err := connBuf.ReadString('\n')
-		if len(str)>0 {
+		if len(str) > 0 {
 			fmt.Println(str)
 		}
-		if err!= nil {
-			fmt.Println("\t runner "+strconv.Itoa(id)+" got its connection closed")
+		if err != nil {
+			fmt.Println("\t runner " + strconv.Itoa(id) + " got its connection closed")
 			wg.Done()
 			return err
 		}
 	}
 }
 
-func run_threads(numberConnections int, delay int, host string, port int) {
+func runThreads(numberConnections int, delay int, host string, port int) {
 	runtime.GOMAXPROCS(numberConnections)
 
 	var wg sync.WaitGroup
 	wg.Add(numberConnections)
 
-	for runner:= 1; runner <= numberConnections ; runner++ {
-		fmt.Println("Initiating runner # "+strconv.Itoa(runner))
-		go connection_handler(runner, host, port, &wg)
+	for runner := 1; runner <= numberConnections; runner++ {
+		fmt.Println("Initiating runner # " + strconv.Itoa(runner))
+		go connectionHandler(runner, host, port, &wg)
 		time.Sleep(time.Duration(delay) * time.Millisecond)
-		fmt.Println("Runner "+strconv.Itoa(runner)+" initated. Remaining: "+strconv.Itoa(numberConnections-runner))
+		fmt.Println("Runner " + strconv.Itoa(runner) + " initated. Remaining: " + strconv.Itoa(numberConnections-runner))
 	}
 
 	fmt.Println("Waiting runners to finish")
 	wg.Wait()
 }
 
-var VersionString = "unset"
+var versionString = "unset"
 
 func main() {
 	hostPtr := flag.String("host", "localhost", "Host you want to open tcp connections against")
@@ -60,9 +60,9 @@ func main() {
 
 	flag.Parse()
 
-	fmt.Println("Running Version: ", VersionString)
-	
-	run_threads(*numberConnectionsPtr, *delayPtr, *hostPtr, *portPtr )
+	fmt.Println("Running Version: ", versionString)
+
+	runThreads(*numberConnectionsPtr, *delayPtr, *hostPtr, *portPtr)
 
 	fmt.Println("\nTerminating Program")
 }
