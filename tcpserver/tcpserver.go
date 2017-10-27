7ce11acdfba38518e1b4f020332cdd07de357f59
Christian Adell Querol <christian.adell@schibsted.com>
2017-10-27 10:35:47 +0200

    Add proper descriptions

--- tcpserver/tcpserver.go
+++ tcpserver/tcpserver.go
@@ -10,12 +10,14 @@ import (
 	"time"
 )
 
-type Handler struct { // Handler : describe what this function does
+// Handler : Struct
+type Handler struct {
 	conn   net.Conn
 	closed chan bool
 }
 
-func (h *Handler) Listen() { // Listen : listen connection for incomming data
+// Listen : listen connection for incomming data
+func (h *Handler) listen() {
 	defer h.conn.Close()
 	bf := bufio.NewReader(h.conn)
 	for {
@@ -31,21 +33,23 @@ func (h *Handler) Listen() { // Listen : listen connection for incomming data
 	}
 }
 
-type Dispatcher struct { // Dispatcher : describe what this function does
+// Dispatcher : Struct with all the hanlders
+type Dispatcher struct {
 	Handlers map[string]*Handler //`type:"map[ip]*Handler"`
 }
 
-func (d *Dispatcher) AddHandler(conn net.Conn) { // AddHandler : describe what this function does
+func (d *Dispatcher) addHandler(conn net.Conn) {
 	addr := conn.RemoteAddr().String()
 	handler := &Handler{conn, make(chan bool, 1)}
 	d.Handlers[addr] = handler
 
-	go handler.Listen()
+	go handler.listen()
 
 	<-handler.closed // when connection closed, remove handler from handlers
 	delete(d.Handlers, addr)
 }
 
+// ListenHandlers : start listening on the handler
 func (d *Dispatcher) ListenHandlers(port int) error {
 	sport := strconv.Itoa(port)
 
@@ -69,6 +73,6 @@ func (d *Dispatcher) ListenHandlers(port int) error {
 		tcpconn.SetKeepAlive(true)
 		tcpconn.SetKeepAlivePeriod(10 * time.Second)
 
-		go d.AddHandler(conn)
+		go d.addHandler(conn)
 	}
 }
