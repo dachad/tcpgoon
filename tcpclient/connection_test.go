76bb8e27e75c581b219a02378479015a5258487a
Christian Adell Querol <chadell@gmail.com>
2017-12-08 15:07:55 +0100

    Get rid of getters tests

--- tcpclient/connection_test.go
+++ tcpclient/connection_test.go
@@ -6,41 +6,6 @@ import (
 	"time"
 )
 
-func TestGetConnectionStatus(t *testing.T) {
-	connectionDescription := Connection{
-		ID:      0,
-		status:  ConnectionDialing,
-		metrics: connectionMetrics{},
-	}
-	if connectionDescription.GetConnectionStatus() != ConnectionDialing {
-		t.Error("GetConnectionStatus is not working")
-	}
-}
-
-func TestGetTCPProcessingDurationEstablished(t *testing.T) {
-
-	connectionDescription := Connection{
-		ID:      0,
-		status:  ConnectionEstablished,
-		metrics: connectionMetrics{tcpEstablishedDuration: 1 * time.Second},
-	}
-	if connectionDescription.GetTCPProcessingDuration(ConnectionEstablished) != 1*time.Second {
-		t.Error("GetTCPProcessingDuration is not working")
-	}
-}
-
-func TestGetTCPProcessingDurationErrored(t *testing.T) {
-
-	connectionDescription := Connection{
-		ID:      0,
-		status:  ConnectionError,
-		metrics: connectionMetrics{tcpErroredDuration: 1 * time.Second},
-	}
-	if connectionDescription.GetTCPProcessingDuration(ConnectionError) != 1*time.Second {
-		t.Error("GetTCPProcessingDuration is not working")
-	}
-}
-
 func TestConnectionString(t *testing.T) {
 
 	connectionDescription := Connection{
