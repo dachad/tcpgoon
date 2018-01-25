0371ae801e2d2c3f870b90ff8d87c88a4edb2439
Christian Adell Querol <chadell@gmail.com>
2018-01-25 07:33:35 +0100

    Change params attribute names to make them clear

--- cmd/tcpgoon.go
+++ cmd/tcpgoon.go
@@ -15,15 +15,15 @@ import (
 )
 
 type tcpgoonParams struct {
-	hostPtr              string
-	hostName             string
-	portPtr              int
-	numberConnectionsPtr int
-	delayPtr             int
-	connDialTimeoutPtr   int
-	debugPtr             bool
-	reportingIntervalPtr int
-	assumeyesPtr         bool
+	target            string
+	targetip          string
+	port              int
+	numberConnections int
+	delay             int
+	connDialTimeout   int
+	debug             bool
+	reportingInterval int
+	assumeyes         bool
 }
 
 var params tcpgoonParams
@@ -47,59 +47,59 @@ var runCmd = &cobra.Command{
 }
 
 func init() {
-	runCmd.Flags().IntVarP(&params.numberConnectionsPtr, "connections", "c", 100, "Number of connections you want to open")
-	runCmd.Flags().IntVarP(&params.delayPtr, "sleep", "s", 10, "Time you want to sleep between connections, in ms")
-	runCmd.Flags().IntVarP(&params.connDialTimeoutPtr, "dial-timeout", "t", 5000, "Connection dialing timeout, in ms")
-	runCmd.Flags().BoolVarP(&params.debugPtr, "debug", "d", false, "Print debugging information to the standard error")
-	runCmd.Flags().IntVarP(&params.reportingIntervalPtr, "interval", "i", 1, "Interval, in seconds, between stats updates")
-	runCmd.Flags().BoolVarP(&params.assumeyesPtr, "assume-yes", "y", false, "Force execution without asking for confirmation")
+	runCmd.Flags().IntVarP(&params.numberConnections, "connections", "c", 100, "Number of connections you want to open")
+	runCmd.Flags().IntVarP(&params.delay, "sleep", "s", 10, "Time you want to sleep between connections, in ms")
+	runCmd.Flags().IntVarP(&params.connDialTimeout, "dial-timeout", "t", 5000, "Connection dialing timeout, in ms")
+	runCmd.Flags().BoolVarP(&params.debug, "debug", "d", false, "Print debugging information to the standard error")
+	runCmd.Flags().IntVarP(&params.reportingInterval, "interval", "i", 1, "Interval, in seconds, between stats updates")
+	runCmd.Flags().BoolVarP(&params.assumeyes, "assume-yes", "y", false, "Force execution without asking for confirmation")
 }
 
 func validateRequiredArgs(params *tcpgoonParams, args []string) error {
 	if len(args) != 2 {
 		return errors.New("Number of required parameters doesn't match")
 	}
-	params.hostName = args[0]
-	addrs, err := net.LookupIP(params.hostName)
+	params.target = args[0]
+	addrs, err := net.LookupIP(params.target)
 	if err != nil || len(addrs) == 0 {
 		return errors.New("Domain name not resolvable")
 	}
 
-	params.hostPtr = addrs[0].String()
-	fmt.Fprintln(debugging.DebugOut, "TCPGOON target: Hostname(", params.hostName, "), IP (", params.hostPtr, ")")
+	params.targetip = addrs[0].String()
+	fmt.Fprintln(debugging.DebugOut, "TCPGOON target: Hostname(", params.target, "), IP (", params.targetip, ")")
 
 	port, err := strconv.Atoi(args[1])
 	if err != nil && port <= 0 {
 		return errors.New("Port argument is not a valid integer")
 	}
-	params.portPtr = port
+	params.port = port
 
 	return nil
 }
 
 func enableDebuggingIfFlagSet(params tcpgoonParams) {
-	if params.debugPtr {
+	if params.debug {
 		debugging.EnableDebug()
 	}
 }
 
 func autorunValidation(params tcpgoonParams) {
-	if !(params.assumeyesPtr || cmdutil.AskForUserConfirmation(params.hostName, params.portPtr, params.numberConnectionsPtr)) {
+	if !(params.assumeyes || cmdutil.AskForUserConfirmation(params.target, params.port, params.numberConnections)) {
 		fmt.Fprintln(debugging.DebugOut, "Execution not approved by the user")
 		cmdutil.CloseAbruptly()
 	}
 }
 
 func run(params tcpgoonParams) {
-	tcpclient.DefaultDialTimeoutInMs = params.connDialTimeoutPtr
+	tcpclient.DefaultDialTimeoutInMs = params.connDialTimeout
 
 	// TODO: we should decouple the caller from the mtcpclient package (too many structures being moved from
 	//  one side to the other.. everything in a single structure, or applying something like the builder pattern,
 	//  may help
-	connStatusCh, connStatusTracker := mtcpclient.StartBackgroundReporting(params.numberConnectionsPtr, params.reportingIntervalPtr)
+	connStatusCh, connStatusTracker := mtcpclient.StartBackgroundReporting(params.numberConnections, params.reportingInterval)
 	closureCh := mtcpclient.StartBackgroundClosureTrigger(connStatusTracker)
-	mtcpclient.MultiTCPConnect(params.numberConnectionsPtr, params.delayPtr, params.hostPtr, params.portPtr, connStatusCh, closureCh)
+	mtcpclient.MultiTCPConnect(params.numberConnections, params.delay, params.target, params.port, connStatusCh, closureCh)
 	fmt.Fprintln(debugging.DebugOut, "Tests execution completed")
 
-	cmdutil.CloseNicely(params.hostPtr, params.hostName, params.portPtr, connStatusTracker)
+	cmdutil.CloseNicely(params.targetip, params.target, params.port, connStatusTracker)
 }
